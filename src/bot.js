7e6a90f4feabb5785e453a3f56ea0dfa02376eab
Edward Pasenidis <me@edpasenidis.tech>
2020-06-03 13:34:17 +0300

    feat: generate random tokens each time (nanoid)

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,17 @@
 const { Client } = require('discord.js')
+const { nanoid } = require('nanoid');
 const WS = require('./ws/ws')
 const dotenv = require('dotenv')
 
-let client = new Client();
+let client = new Client()
+let _token = nanoid()
 dotenv.config()
 
-let ws = new WS('bf56d570e62cdb573c86b18526296117', 5655, client)
+let ws = new WS(_token, 5655, client)
 
 client.on('ready', () => {
     console.log(`Bot is logged in as ${client.user.tag}`)
+    console.log(`Nanoid Session Token: ${_token}`)
 })
 
 client.login(process.env.BOT_TOKEN)
